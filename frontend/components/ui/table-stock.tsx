0ddee2fc7f48ff39d89ce1a5de99c11a19d57eec
Raihan <raihanregita@gmail.com>
2026-07-06 11:00:15 +0700

    bug kecil setPage

--- frontend/components/ui/table-stock.tsx
+++ frontend/components/ui/table-stock.tsx
@@ -47,7 +47,6 @@ export default function TableStock({
                         value={search}
                         onChange={(value) => {
                             setSearch(value);
-                            setPage?.(1);
                         }}
                         placeholder="Cari nama barang..."
                     />
