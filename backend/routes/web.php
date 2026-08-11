e139aaaf25e10632c8b466a184b37754c5943bc9
Raihan <raihanregita@gmail.com>
2026-08-11 12:00:01 +0700

    temp: add one-time seed trigger route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,3 +7,8 @@ Route::get('/', function () {
         'status' => 'Laravel Running'
     ]);
 });
+
+Route::get('/run-seed-once-bwO9LXTACp', function () {
+    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+    return 'Seeded!';
+});
