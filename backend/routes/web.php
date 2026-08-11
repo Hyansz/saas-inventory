377a68fbfc27dd6263542d843732e5f4444d45d7
Raihan <raihanregita@gmail.com>
2026-08-11 13:08:43 +0700

    chore: remove temporary seed trigger route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,8 +7,3 @@ Route::get('/', function () {
         'status' => 'Laravel Running'
     ]);
 });
-
-Route::get('/run-seed-once-bwO9LXTACp', function () {
-    \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
-    return 'Seeded!';
-});
